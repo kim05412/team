ea888396c77dfd0409e60026a6e5031b245dbd40
Sohyun kim <rlathgus541@naver.com>
2023-10-10 11:46:04 +0900

    first

---
+++ src/main/kotlin/com/example/team/TeamApplication.kt
@@ -0,0 +1,11 @@
+package com.example.team
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamApplication
+
+fun main(args: Array<String>) {
+	runApplication<TeamApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/team/TeamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.team
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
