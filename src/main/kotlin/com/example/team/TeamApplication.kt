294fa23d369998b18082d728dbdf3eea18fcd1d0
Sohyun kim <rlathgus541@naver.com>
2023-10-16 12:44:50 +0900

    파일구조 변경

--- src/main/kotlin/com/example/team/TeamApplication.kt
+++ src/main/kotlin/com/example/team/TeamApplication.kt
@@ -3,10 +3,8 @@ package com.example.team
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
